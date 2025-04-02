b75ad3293fe8ed8b417c3a8fcbe56543a0041afc
matiasmachadoupc <matias.machado@estudiantat.upc.edu>
2025-04-02 16:12:55 +0200

    Email Robusto

--- src/modules/users/user_models.ts
+++ src/modules/users/user_models.ts
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true, // Asegura que el correo electrónico sea único
-        match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ // Valida la estructura del correo electrónico
+        match: /^[^\s@]+@(gmail|yahoo|hotmail|outlook|icloud|protonmail)\.(com|es|org|net|edu|gov|info|io|co|us|uk)$/i // Valida proveedores y dominios comunes
     },
     password: {
         type: String,
